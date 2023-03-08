8e4c51961ac18f53edba3dc2d34fb40a2dd281ba
Clarice Tran <tran.clarice@gmail.com>
2023-03-08 10:30:23 -0800

    temp save to merge changes

--- happytappers/src/App.js
+++ happytappers/src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-const io = require("socket.io-client");
 import Home from "./components/pages/Home";
 import Room from "./components/pages/Room";
+
+const io = require("socket.io-client");
+
 function App() {
-    const socket = io("http://localhost:8080", {
+    const socket = io("http://localhost:8080", {});
+    socket.on("connection", () => {
+        console.log(`I'm connected with the back-end`);
     });
-      socket.on('connection', () => {
-          console.log(`I'm connected with the back-end`);
-  });
     return (
         <BrowserRouter>
             <Routes>
