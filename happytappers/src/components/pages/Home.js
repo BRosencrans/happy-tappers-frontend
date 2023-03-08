1d98e9b9d73286f65b725c5830c875ce70d1926c
Febin George <you@example.com>
2023-03-08 13:48:56 -0800

    updates

--- happytappers/src/components/pages/Home.js
+++ happytappers/src/components/pages/Home.js
@@ -1,9 +1,41 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
 
 export default function Home() {
     return (
-        <div>
+        <Container fluid className="vh-100">
+                    <h1 className='p-4 m-5 text-center'>Happy Tappers</h1>
+      <Row>
+        <br/>
+        <Col className='text-center'>
+          <Card className='p-4 m-5'>
+          <div className="mb-2">
+                <Button variant="primary" size="lg">
+                    Login
+                </Button>{' '}
+            </div>
+          </Card>
+        </Col>
+        <Col className='text-center'>
+          <Card className='p-4 m-5'>
+            <div className="mb-2">
+                <Button variant="primary" size="lg">
+                    Signup
+                </Button>{' '}
+            </div>
+        </Card>
+        </Col>
+      </Row>
+      <br/>
+      <Card className='p-4 m-5'>
+        <h3 className='text-center'>High Score</h3>
+      </Card>
             <form>
                 <input type="text" minlength="4" />
                 <NavLink to="/room">
@@ -13,6 +45,6 @@ export default function Home() {
             <NavLink to="/room">
                 <button>Create Room</button>
             </NavLink>
-        </div>
+        </Container>
     );
 }
