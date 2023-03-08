5f946080a0db954c378ac4cbad0878ebab78fc2e
Febin George <you@example.com>
2023-03-08 12:22:08 -0800

    updates

--- happytappers/src/App.js
+++ happytappers/src/App.js
@@ -1,19 +1,9 @@
+import { Home } from "./components/Pages/Home";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Home/>
     </div>
   );
 }
