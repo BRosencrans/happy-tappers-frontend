064664239d1e773074482b2579ee489630e9dbd1
Clarice Tran <tran.clarice@gmail.com>
2023-03-12 15:26:50 -0700

    Moved game into its own component for easier breakdown for socket logic. Created global socket so only one socket id is being used per tab

---
+++ happytappers/src/utils/SocketHelper.js
@@ -0,0 +1,5 @@
+import React from "react";
+import io from "socket.io-client";
+
+export const socket = io.connect("http://localhost:8080");
+export const SocketContext = React.createContext(socket);
