ad556e2607cef8aa3e1222017da2db0929f6a4ef
BRosencrans <brandon.rosencrans@gmail.com>
2023-03-11 19:24:50 -0800

    added signup route to db from frontend

--- happytappers/src/components/pages/Signup.js
+++ happytappers/src/components/pages/Signup.js
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import { Container, Form, Button, Row, Col } from "react-bootstrap";
 import HomeButton from "../HomeButton";
 
-export default function Signup() {
+export default class Signup extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            username: "",
+            password:""
+        }
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+    handleSubmit(e){
+        e.preventDefault()
+        const{username, password} = this.state;
+        console.log(username)
+        
+fetch("http://localhost:3001/api/users/signup", {
+    method: "POST",
+    crossDomain: true,
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      "Access-Control-Allow-Origin": "http://localhost:3001/"
+    },
+    body:JSON.stringify({
+        username,
+        password
+    }),
+}).then ((res)=>res.json())
+.then((data)=>{
+    console.log (data, "registered successfully")
+})
+    }
+render(){
     return (
         <Container fluid>
             <Row>
@@ -12,19 +43,19 @@ export default function Signup() {
             </Row>
             <Row className="justify-content-md-center">
                 <Col xs lg="4">
-                    <Form>
+                    <Form onSubmit={this.handleSubmit} >
                         <Form.Floating className="mb-3">
-                            <Form.Control id="floatingInputCustom" type="text" placeholder="username"></Form.Control>
+                            <Form.Control id="floatingInputCustom" type="text" placeholder="username" onChange={e=>this.setState({username: e.target.value})}></Form.Control>
                             <label htmlFor="floatingInputCustom">Username</label>
                         </Form.Floating>
 
                         <Form.Floating className="mb-3">
-                            <Form.Control id="floatingPasswordCustom" type="password" placeholder="password" />
+                            <Form.Control id="floatingPasswordCustom" type="password" placeholder="password"/>
                             <label htmlFor="floatingPasswordCustom">Password</label>
                         </Form.Floating>
 
                         <Form.Floating className="mb-3">
-                            <Form.Control id="floatingPasswordCustom" type="password" placeholder="password" />
+                            <Form.Control id="floatingPasswordCustom" type="password" placeholder="password" onChange={e=>this.setState({ password: e.target.value})}/>
                             <label htmlFor="floatingPasswordCustom">Confirm Password</label>
                         </Form.Floating>
 
@@ -35,5 +66,5 @@ export default function Signup() {
                 </Col>
             </Row>
         </Container>
-    );
+    );}
 }
