c9f5a8a5a6dcdf6f2a75537b24a14973422df79d
BRosencrans <brandon.rosencrans@gmail.com>
2023-03-12 17:51:01 -0700

    logout working

--- happytappers/src/components/LogoutButton.js
+++ happytappers/src/components/LogoutButton.js
@@ -3,10 +3,14 @@ import { NavLink } from "react-router-dom";
 import { Button } from "react-bootstrap";
 
 export default function LogoutButton() {
+    const logOut = () => {
+        window.localStorage.clear();
+        window.location.href = "./";
+      };
     return (
         <NavLink to="/">
             {/* TODO: make logout functionality work */}
-            <Button variant="primary" size="lg">
+            <Button onClick={logOut} variant="primary" size="lg">
                 Logout
             </Button>
         </NavLink>
