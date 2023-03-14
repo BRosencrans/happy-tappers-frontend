d6302ce5ed290e6aa87eebdb60371febf493bb91
BRosencrans <brandon.rosencrans@gmail.com>
2023-03-13 19:04:07 -0700

    deploy bug fix

--- happytappers/src/utils/SocketHelper.js
+++ happytappers/src/utils/SocketHelper.js
@@ -1,5 +1,5 @@
 import React from "react";
 import io from "socket.io-client";
 
-export const socket = io.connect("https://happytapperssockets.herokuapp.com/");
+export const socket = io.connect("https://happytapperssockets.herokuapp.com");
 export const SocketContext = React.createContext(socket);
